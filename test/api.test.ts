c3368c6cafb55b40e6af193e73ac29818f985478
Carlos Andres Vargas Lopez <cavargas40@gmail.com>
2020-03-11 16:40:18 -0500

    async testing & http-status

--- test/api.test.ts
+++ test/api.test.ts
@@ -1,10 +1,20 @@
 import request from 'supertest';
 import app from '../src/app';
+import { OK } from 'http-status';
 
 describe('GET /', () => {
   it('should return 200 OK', () => {
     return request(app)
       .get('/')
-      .expect(200);
+      .expect(OK);
+  });
+
+  it('should check body of the request', async done => {
+    const response = await request(app).get('/');
+
+    expect(response.status).toBe(OK);
+    expect(response.body.message).toBe('Hello World!');
+    
+    done();
   });
 });
